7e7819d6d985ab6c443df1c8c1990035edb9c089
xiaoxy <xiaoxy@guahao.com>
2020-09-10 13:12:15 +0800

    build

---
+++ we-code/build/precache-manifest.0b5d84f6f40418b352367a0681c1eb9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f77cb85ff6151cadee79712e693fc86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28bcd1ae245aad504412",
+    "url": "/static/css/main.aa464197.chunk.css"
+  },
+  {
+    "revision": "d306cf01e4b0a7bdb081",
+    "url": "/static/js/2.caf801a7.chunk.js"
+  },
+  {
+    "revision": "b0768b210f6a5e5838f8bbab7247495e",
+    "url": "/static/js/2.caf801a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28bcd1ae245aad504412",
+    "url": "/static/js/main.72819528.chunk.js"
+  },
+  {
+    "revision": "cc4c596a5e3fae618cef",
+    "url": "/static/js/runtime-main.518ce9f8.js"
+  }
+]);
