ebed7e689974fd204a4ef856019f9e7e02c67eef
xyxiao001 <463618622@qq.com>
2020-10-29 22:00:43 +0800

    1

--- course/tree/levelOrder.js
+++ course/tree/levelOrder.js
@@ -19,25 +19,22 @@ var levelOrder = function (root) {
   if (!root) {
     return ret
   }
-  const stack = []
-  stack.push(root)
-  while (stack.length !== 0) {
-    const currentLevelSize = stack.length
+  const q = []
+  q.push(root)
+  while (q.length !== 0) {
+    const currentLevelSize = q.length
+    ret.push([])
     for (let i = 1; i <= currentLevelSize; i++) {
-      // 变量 i 无实际意义，只是为了循环 n 次
-      // 将当前层的所有结点出队列，再将下一层的所有结点入队列
-      const node = stack.shift()
-      ret.push(node.val)
-      if (node.left) stack.push(node.left)
-      if (node.right) stack.push(node.right)
+      const node = q.shift()
+      ret[ret.length - 1].push(node.val)
+      if (node.left) q.push(node.left)
+      if (node.right) q.push(node.right)
     }
   }
 
   return ret
 }
 
-console.log(`层序遍历结果 A,B,C,D,F,G,I,E,H`)
-
 console.log(
-  `层序广度优先遍历 ${levelOrder(treeData)}`
+  `层序遍历: ${levelOrder(treeData)}`
 )
